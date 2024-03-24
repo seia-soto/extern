1c8c8dcdac957691f2cbf1fa04fa6eedcd0a1404
HoJeong Go <seia@outlook.kr>
2024-03-24 11:17:38 +0900

    docs: document ErRouteResource

--- packages/er-api/src/types/utils.ts
+++ packages/er-api/src/types/utils.ts
@@ -2,6 +2,10 @@ export type Numeric = `${number}`;
 
 export type DateTime = `${number}-${number}-${number}T${number}:${number}:${number}.${number}+${number}`;
 
+/** An integer value indicating seconds */
 export type Seconds = number;
+
+/** A number value between zero to one */
+export type Ratio = number;
 
 export type JsonLiteral<T = unknown> = string | T;
